ff8f8b1ecf3981144f8c6cb338a7d4a05251fcc0
Burak Karademir <94866565+blackIronn@users.noreply.github.com>
2025-10-30 17:21:20 +0300

    add: remove todo

--- src/pages/CreateTodo.tsx
+++ src/pages/CreateTodo.tsx
@@ -1,13 +1,41 @@
-
+import { useState } from "react";
+import api from "../api/api";
 
 function CreateTodo() {
-  
+  const [newTodo, setNewTodo] = useState("");
+
+  const createTodo = async () => {
+    if (!newTodo.trim()) return;
+    await api.post("/todos", {
+      title: newTodo,
+      status: "todo",
+    });
+    setNewTodo("");
+  };
 
   return (
-    <div>
-      input koycam json db yazcam yarın***
+    <div className="flex-col justify-center">
+      <div className="mt-20 flex justify-center text-2xl">
+        <h1>CREATE TODO</h1>
+      </div>
+
+      <div className="flex justify-center mt-20 text-black gap-5">
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="New Todo..."
+          className="rounded-lg w-96 px-3 py-2"
+        />
+        <button
+          onClick={createTodo}
+          className="text-white bg-[#181818] px-4 py-2 rounded-lg hover:bg-gray-600 transition"
+        >
+          Create
+        </button>
+      </div>
     </div>
   );
 }
 
-export default CreateTodo
+export default CreateTodo;
--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -13,15 +13,51 @@ function Todos() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   useEffect(() => {
-    api.get("/todos").then(res => setTodos(res.data));
+    getTodos();
   }, []);
 
+  const getTodos = () => {
+    api.get("/todos").then(res => setTodos(res.data));
+  }
+
+  const Update = () => {
+
+  }
+
+  const ChangeStatus = () => {
+    
+  }
+  
+  const Remove = async (id: number) => {
+    const remove = await api.delete(`/todos/${id}`);
+    getTodos();
+  }
+
   return (
-    <div>
+    <div className='flex-col justify-center text-center pt-20'>
       {todos
         .filter(item => item.status === "todo")
-        .map((item, index) => (
-          <p key={index}>{item.title}</p>
+        .map((item) => (
+          <div className='flex justify-center'>
+            <div className=' py-5 px-5 bg-[#181818] mt-5 w-4/5 mx-auto rounded-lg text-2xl'>
+              <p key={item.id}>{item.title}</p>
+            </div>
+            <div className='flex justify-center gap-5 pl-5'>
+              <button 
+                className='py-5 px-5 bg-[#6a0a97] mt-5 mx-auto rounded-lg text-2xl'>
+                Update
+              </button>
+              <button 
+                className='py-5 px-5 bg-[#138a23] mt-5 mx-auto rounded-lg text-2xl'>
+                Change Status
+              </button>
+              <button 
+                className='py-5 px-5 bg-[#831818] mt-5 mx-auto rounded-lg text-2xl'
+                onClick={() => Remove(item.id)}>
+                Remove
+              </button>
+            </div>
+          </div>
         ))}
     </div>
   );
