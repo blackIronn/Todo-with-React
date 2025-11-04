8c7b327c68a5e3c4dd35952ba9df9d0209c1ad9c
Burak Karademir <94866565+blackIronn@users.noreply.github.com>
2025-11-04 19:27:31 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,60 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 
 import Todos from "./pages/Todos";
 import InProgress from "./pages/InProgress";
 import Closed from "./pages/Closed";
 import CreateTodo from "./pages/CreateTodo";
 
-
-
-
 function App() {
 
   const classes: string[] = [
-    "text-center text-2xl pt-20 flex justify-center gap-20",
-    "px-4 py-2 rounded-lg bg-[#181818] hover:bg-gray-600 transition"
-  ]
+    "text-center text-xl pt-20 flex justify-center gap-20",
+    "px-4 py-2 rounded-full border border-white transition",
+    "bg-gray-700",
+    "bg-[#181818] hover:bg-gray-700"
+  ];
 
-  const [headerClass, linkClass] = classes;
-
+  const [headerClass, linkClass, active, deactive] = classes;
 
   return (
     <>
       <div>
-        <nav className= {headerClass}>
-          <Link className = {linkClass} to="/">Create Todo</Link>
-          <Link className = {linkClass} to="/todos">Todos</Link>
-          <Link className = {linkClass} to="/inProgress">In Progress</Link>
-          <Link className = {linkClass} to="/closed">Closed</Link>
+        <nav className={headerClass}>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `${linkClass} ${isActive ? active : deactive}`
+            }
+          >
+            Create Todo
+          </NavLink>
+
+          <NavLink
+            to="/todos"
+            className={({ isActive }) =>
+              `${linkClass} ${isActive ? active : deactive}`
+            }
+          >
+            Todos
+          </NavLink>
+
+          <NavLink
+            to="/inProgress"
+            className={({ isActive }) =>
+              `${linkClass} ${isActive ? active : deactive}`
+            }
+          >
+            In Progress
+          </NavLink>
+
+          <NavLink
+            to="/closed"
+            className={({ isActive }) =>
+              `${linkClass} ${isActive ? active : deactive}`
+            }
+          >
+            Closed
+          </NavLink>
         </nav>
 
         <Routes>
@@ -34,9 +63,9 @@ function App() {
           <Route path="/inProgress" element={<InProgress />} />
           <Route path="/closed" element={<Closed />} />
         </Routes>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
